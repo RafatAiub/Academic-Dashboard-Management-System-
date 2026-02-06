9a6a65a2babe191274aad3654d82a0c00eb8ab04
RafatAiub <tanvirmahtab1996@gmail.com>
2026-02-06 10:11:03 +0600

    remove sidebar from the portal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Sidebar from "@/components/layout/Sidebar";
 import Header from "@/components/layout/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="flex min-h-screen bg-white">
-      <Sidebar />
       <div className="flex-1 bg-gray-50">
         <Header />
         <Component {...pageProps} />
